c302072eeca76d614b3f8a2539372b514a9a38ad
sefx <35553438+sefx@users.noreply.github.com>
2024-09-02 09:30:45 -0700

    provider: Use abstractmethod to require subclasses adhere to interface.

--- cataplot/providers/base.py
+++ cataplot/providers/base.py
@@ -1,16 +1,19 @@
 """
 Base class for providers that provide data to be plotted.
 """
-
+from abc import ABC, abstractmethod
 from datetime import datetime
 
-class BaseProvider:
+class BaseProvider(ABC):
     """
     Base class for providers that provide data to be plotted.
     """
-    def __init__(self):
-        pass
+    def __init__(self, name:str, description:str='', cfg:dict=None):
+        self.name = name
+        self.description = description
+        self.cfg = cfg or {}
 
+    @abstractmethod
     def listdir(self, path:str='/') -> list[tuple[str, str, str]]:
         """
         Returns a list of (name, type, description) items in the specified path.
@@ -26,6 +29,7 @@ class BaseProvider:
         """
         raise NotImplementedError
 
+    @abstractmethod
     def get_data(self, path: str, start: datetime, end: datetime) -> list:
         """
         Returns a list of values between the start and end times from the
